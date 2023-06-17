a17343949c56447c7961e31e05e86f166e99f965
Leticia Carolina Azevedo <leticiac@email.com>
2023-06-17 15:45:16 +0200

    updated code

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const button = document.querySelector("#button");
 
 //2 when click on submit form return a resultText if right or wrong
 button.addEventListener("click", () => {
-  if (input.value == 87) {
+  if (input.value == 88) {
     resultText.innerText = "Right on the spot! You are a lucky one.";
-  } else if (input.value < 35) {
-    resultText.innerText = "Not there yet";
-  } else if (input.value > 87) {
-    resultText.innerText = "That was not a good guess";
+  } else if (input.value < 20) {
+    resultText.innerText = "You are far away";
+  } else if (input.value > 80) {
+    resultText.innerText = "You are getting close";
   } else {
     resultText.innerText = "Try again";
   }
